160e2b9f130db59539bca37776a56035f27e4572
tyleravochoc <tyler@avochoc.com>
2025-01-23 17:56:22 +0200

    Checkpoint 4
    Implementing company listings view model

---
+++ app/src/main/java/ty/bre/stocklens/presentation/company_listings/CompanyListingsEvent.kt
@@ -0,0 +1,6 @@
+package ty.bre.stocklens.presentation.company_listings
+
+sealed class CompanyListingsEvent {
+    object Refresh : CompanyListingsEvent()
+    data class OnSearchQueryChange(val query: String) : CompanyListingsEvent()
+}
---
+++ app/src/main/java/ty/bre/stocklens/presentation/company_listings/CompanyListingsState.kt
@@ -0,0 +1,10 @@
+package ty.bre.stocklens.presentation.company_listings
+
+import ty.bre.stocklens.domain.model.CompanyListing
+
+data class CompanyListingsState(
+    val companies: List<CompanyListing> = emptyList(),
+    val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = ""
+)
