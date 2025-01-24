06d6c7d79238ee37f182bd3190858239cad86df1
tyleravochoc <tyler@avochoc.com>
2025-01-24 15:55:46 +0200

    Adding a draggable FAB

--- app/src/main/java/ty/bre/stocklens/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/ty/bre/stocklens/presentation/company_listings/CompanyListingsScreen.kt
@@ -2,24 +2,36 @@ package ty.bre.stocklens.presentation.company_listings
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import ty.bre.stocklens.R
+import kotlin.math.roundToInt
 
 @Composable
 fun CompanyListingsScreen(
@@ -29,50 +41,76 @@ fun CompanyListingsScreen(
     val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = viewModel.state.isRefreshing)
     val state = viewModel.state
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        OutlinedTextField(
-            value = state.searchQuery,
-            onValueChange = {
-                viewModel.onEvent(CompanyListingsEvent.OnSearchQueryChange(it))
-            },
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            placeholder = {
-                Text(text = "Search...")
-            },
-            leadingIcon = {
-                Image(
-                    painter = painterResource(id = R.drawable.search),
-                    contentDescription = "Search"
-                )
-            },
-            maxLines = 1,
-            singleLine = true
-        )
-        SwipeRefresh(
-            state = swipeRefreshState,
-            onRefresh = {
-                viewModel.onEvent(CompanyListingsEvent.Refresh)
-            }
+    // State to track the FABs position
+    var fabOffsetX by remember { mutableFloatStateOf(0f) }
+    var fabOffsetY by remember { mutableFloatStateOf(0f) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize()
         ) {
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(state.companies.size) { i ->
-                    val company = state.companies[i]
-                    CompanyItem(
-                        company = company, modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {}
-                            .padding(16.dp)
+            OutlinedTextField(
+                value = state.searchQuery,
+                onValueChange = {
+                    viewModel.onEvent(CompanyListingsEvent.OnSearchQueryChange(it))
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                placeholder = {
+                    Text(text = "Search...")
+                },
+                leadingIcon = {
+                    Image(
+                        painter = painterResource(id = R.drawable.search),
+                        contentDescription = "Search"
                     )
-                    if (i < state.companies.size) {
-                        HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+                },
+                maxLines = 1,
+                singleLine = true
+            )
+            SwipeRefresh(
+                state = swipeRefreshState,
+                onRefresh = {
+                    viewModel.onEvent(CompanyListingsEvent.Refresh)
+                }
+            ) {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(state.companies.size) { i ->
+                        val company = state.companies[i]
+                        CompanyItem(
+                            company = company, modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {}
+                                .padding(16.dp)
+                        )
+                        if (i < state.companies.size) {
+                            HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+                        }
                     }
                 }
             }
         }
-    }
 
+        // Draggable FAB
+        FloatingActionButton(
+            onClick = { /* Handle FAB click */ },
+            modifier = Modifier
+                .offset { IntOffset(fabOffsetX.roundToInt(), fabOffsetY.roundToInt()) }
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume() // Consume the gesture
+                        fabOffsetX += dragAmount.x
+                        fabOffsetY += dragAmount.y
+                    }
+                }
+                .padding(16.dp),
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.theme_selector),
+                contentDescription = "Draggable FAB",
+                tint = White
+            )
+        }
+    }
 }
