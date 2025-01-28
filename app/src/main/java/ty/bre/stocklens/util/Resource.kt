e988efb4a790330b25f54a83a073ae039b8189d9
tyleravochoc <tyler@avochoc.com>
2025-01-28 12:26:45 +0200

    Some more comments

--- app/src/main/java/ty/bre/stocklens/util/Resource.kt
+++ app/src/main/java/ty/bre/stocklens/util/Resource.kt
@@ -1,5 +1,13 @@
 package ty.bre.stocklens.util
 
+/**
+ * A sealed class representing the state of a resource, used for managing
+ * data fetching operations and handling their success, error, and loading states.
+ *
+ * @param T The type of data associated with the resource.
+ * @property data The optional data being passed with the resource.
+ * @property message An optional message, typically used for error descriptions.
+ */
 sealed class Resource<T>(
     val data: T? = null,
     val message: String? = null
