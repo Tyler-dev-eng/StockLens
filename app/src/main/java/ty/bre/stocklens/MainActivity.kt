1ef62501cc703ba7cfc41c1b9fa162ffeff73a3f
tyleravochoc <tyler@avochoc.com>
2025-01-28 10:42:35 +0200

    Adding app logo

--- app/src/main/java/ty/bre/stocklens/MainActivity.kt
+++ app/src/main/java/ty/bre/stocklens/MainActivity.kt
@@ -14,6 +14,7 @@ import ty.bre.stocklens.util.SetupNavGraph
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             StockLensTheme {
                 val navController = rememberNavController()
